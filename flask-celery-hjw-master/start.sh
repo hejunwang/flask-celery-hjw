26680575b42b615e753b9e0a0d18139eb0f3ca1f
hjw <hejunwang01@126.com>
2021-08-03 18:03:08 +0800

    del

--- flask-celery-hjw-master/start.sh
+++
@@ -1,5 +0,0 @@
-#----------------------------------------------------
-#       启动虚拟环境 ,执行监听任务写入1号文件,目前有问题
-#----------------------------------------------------
-source ../../auto_online_py37_app/bin/activate
-celery -A celery_task.tasks worker --loglevel=INFO >1.log
